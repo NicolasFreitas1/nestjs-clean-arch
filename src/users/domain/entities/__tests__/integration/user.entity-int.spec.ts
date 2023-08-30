3329e1e486b1ae629077477e049b363f26d59066
Nicolas Freitas <nicolas.freitas@agpr5.com>
2023-08-30 16:04:13 -0300

    adding integration test on constructor method

--- src/users/domain/entities/__tests__/integration/user.entity-int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity-int.spec.ts
@@ -25,6 +25,86 @@ describe('User entity integration tests', () => {
       };
 
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        name: 19 as any,
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+    });
+    it('Should throw an error when creating a user with invalid email', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        email: null,
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        email: '',
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        email: 'a'.repeat(256),
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        email: 19 as any,
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+    });
+    it('Should throw an error when creating a user with invalid password', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        password: null,
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        password: '',
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        password: 'a'.repeat(101),
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        password: 19 as any,
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+    });
+    it('Should throw an error when creating a user with invalid createdAt', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        createdAt: '2023' as any,
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        createdAt: 19 as any,
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
     });
   });
 });
