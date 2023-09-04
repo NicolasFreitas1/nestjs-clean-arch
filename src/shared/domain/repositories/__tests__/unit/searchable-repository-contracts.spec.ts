741f2293fcccb9671719937f3bdf2092a2616eef
Nicolas Freitas <nicolas.freitas@agpr5.com>
2023-09-04 10:46:48 -0300

    add search params tests

--- src/shared/domain/repositories/__tests__/unit/searchable-repository-contracts.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/searchable-repository-contracts.spec.ts
@@ -56,6 +56,7 @@ describe('Searchable Repository', () => {
         );
       });
     });
+
     it('perPage prop', () => {
       const sut = new SearchParams();
 
@@ -109,6 +110,175 @@ describe('Searchable Repository', () => {
           param.expected,
         );
       });
+    });
+
+    it('sort prop', () => {
+      const sut = new SearchParams();
+
+      expect(sut.sort).toBeNull();
+
+      const params = [
+        {
+          sort: null as any,
+          expected: null,
+        },
+        {
+          sort: undefined as any,
+          expected: null,
+        },
+        {
+          sort: '' as any,
+          expected: null,
+        },
+        {
+          sort: 'test',
+          expected: 'test',
+        },
+        {
+          sort: 0,
+          expected: '0',
+        },
+        {
+          sort: -1,
+          expected: '-1',
+        },
+        {
+          sort: 5.5,
+          expected: '5.5',
+        },
+        {
+          sort: true,
+          expected: 'true',
+        },
+        {
+          sort: {},
+          expected: '[object Object]',
+        },
+        {
+          sort: 50,
+          expected: '50',
+        },
+      ];
+
+      params.forEach((param) => {
+        expect(new SearchParams({ sort: param.sort }).sort).toEqual(
+          param.expected,
+        );
+      });
+    });
+
+    it('sortDir prop', () => {
+      let sut = new SearchParams();
+      expect(sut.sortDir).toBeNull();
+
+      sut = new SearchParams({ sort: null });
+      expect(sut.sortDir).toBeNull();
+
+      sut = new SearchParams({ sort: undefined });
+      expect(sut.sortDir).toBeNull();
+
+      sut = new SearchParams({ sort: '' });
+      expect(sut.sortDir).toBeNull();
+
+      const params = [
+        {
+          sortDir: null as any,
+          expected: 'desc',
+        },
+        {
+          sortDir: undefined as any,
+          expected: 'desc',
+        },
+        {
+          sortDir: '' as any,
+          expected: 'desc',
+        },
+        {
+          sortDir: 'test',
+          expected: 'desc',
+        },
+        {
+          sortDir: 0,
+          expected: 'desc',
+        },
+        {
+          sortDir: 'desc',
+          expected: 'desc',
+        },
+        {
+          sortDir: 'asc',
+          expected: 'asc',
+        },
+        {
+          sortDir: 'ASC',
+          expected: 'asc',
+        },
+        {
+          sortDir: 'DESC',
+          expected: 'DESC',
+        },
+      ];
+
+      params.forEach((param) => {
+        expect(
+          new SearchParams({ sort: 'field', sortDir: param.sortDir }).sortDir,
+        ).toEqual(param.expected);
+      });
+    });
+  });
+
+  it('filter prop', () => {
+    const sut = new SearchParams();
+
+    expect(sut.filter).toBeNull();
+
+    const params = [
+      {
+        filter: null as any,
+        expected: null,
+      },
+      {
+        filter: undefined as any,
+        expected: null,
+      },
+      {
+        filter: '' as any,
+        expected: null,
+      },
+      {
+        filter: 'test',
+        expected: 'test',
+      },
+      {
+        filter: 0,
+        expected: '0',
+      },
+      {
+        filter: -1,
+        expected: '-1',
+      },
+      {
+        filter: 5.5,
+        expected: '5.5',
+      },
+      {
+        filter: true,
+        expected: 'true',
+      },
+      {
+        filter: {},
+        expected: '[object Object]',
+      },
+      {
+        filter: 50,
+        expected: '50',
+      },
+    ];
+
+    params.forEach((param) => {
+      expect(new SearchParams({ filter: param.filter }).filter).toEqual(
+        param.expected,
+      );
     });
   });
 });
